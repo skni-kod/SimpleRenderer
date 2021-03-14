74c94618a6cdf5e81c4190d0770826b8fa1527b3
Coedo <dziedzicpm@gmail.com>
2021-03-14 17:19:40 +0100

    Added auto call to vcvarsll

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @echo off
 
+if NOT "%Platform%" == "X64" IF NOT "%Platform%" == "x64" (call vcvarsall x64)
+
 set exe_name=SimpleRenderer
 set compile_flags= -nologo /Zi /FC /I ../include/ /W3
 set linker_flags= glfw3dll.lib gdi32.lib user32.lib kernel32.lib opengl32.lib /INCREMENTAL:NO
